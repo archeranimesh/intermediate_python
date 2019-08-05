c7cb8c21d4715afc1cfb95084665bd941fb11956
Animesh Kumar Bhadra <animeshb@archerimagine.com>
2019-08-05 23:07:27 +0530

    Try and except block.

---
+++ code/04_exception/02_try_except.py
@@ -0,0 +1,19 @@
+user_input = "a"
+try:
+    int(user_input)
+    print(
+        "int(user_input): ", int(user_input)
+    )  # This line is not printed as there is exception.
+except ValueError:
+    print("Not a number")
+
+d = {1: 1}
+user_input = 2
+
+try:
+    int(user_input)
+    print("int(user_input): ", int(user_input))
+    d[user_input]
+except (ValueError, KeyError) as e:  # Identifier for the exception
+    # Catching 2 exception in one block.
+    print("The error is ", e)
