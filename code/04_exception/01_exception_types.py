99d2e97b39a6ceef03414816b69347afa05ed94d
Animesh Kumar Bhadra <animeshb@archerimagine.com>
2019-08-04 20:00:00 -0600

    Starting Exceptions

---
+++ code/04_exception/01_exception_types.py
@@ -0,0 +1,4 @@
+# Unhandled exceptions are error in code.
+# We should handle these.
+# BaseException is the base class, we should never catch this exception.
+# Exception is the sub class of BaseException which we should catch.
