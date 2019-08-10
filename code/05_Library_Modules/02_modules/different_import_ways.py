c3aa36d0dc70b047d98bc994de4773f019e7d482
archeranimesh <animeshb@archerimagine.com>
2019-08-10 21:52:07 +0530

    Different ways of importing modules.

---
+++ code/05_Library_Modules/02_modules/different_import_ways.py
@@ -0,0 +1,10 @@
+# We can import in these 3 ways.
+# This below import is valid, but should be avoided
+# The below import clutters existing namespace.
+
+#from my_math_function import *
+#print(add_number(1,2))
+
+# The below approach is more cleaner.
+from my_math_function import add_number
+print(add_number(1,2))
