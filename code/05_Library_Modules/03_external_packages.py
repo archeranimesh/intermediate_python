115fa42ef30c583037df7f14d02212d396772919
archeranimesh <animeshb@archerimagine.com>
2019-08-15 22:16:31 +0530

    safety precausion for importing external modules.

---
+++ code/05_Library_Modules/03_external_packages.py
@@ -0,0 +1,5 @@
+# https://pypi.org/ is a good place to search for packages.
+# Recommended way of using pip install.
+# It installs the correct packages based on the python environment.
+# python -m pip install requests
+# Do not misspell the library name, as there are chances to install malicious packages.
