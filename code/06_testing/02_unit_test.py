61c019cdb983a10b9c34f7daea27b6e14af442bf
archeranimesh <animeshb@archerimagine.com>
2019-08-17 22:03:22 +0530

    comments for few line of code.

--- code/06_testing/02_unit_test.py
+++ code/06_testing/02_unit_test.py
@@ -1,12 +1,17 @@
+# import the class, module to be unit tested.
 import mult
 import unittest
 
+# unit test should always subclass from unittest.TestCase
 class TestMultiply(unittest.TestCase):
+    # test case name should start with test_
     def test_multiply(self):
         test_x = 5
         test_y = 10
+        # assertion is from the unit test assertion file.
         self.assertEqual(mult.multiply(test_x, test_y), 50, msg=f"The multiplication of {test_x} and {test_y} should be {test_x * test_y}")
 
 
-# if __name__ == "__main__":
-#    unittest.main()
+if __name__ == "__main__":
+    # invoke unit test by calling the main() method.
+    unittest.main()
