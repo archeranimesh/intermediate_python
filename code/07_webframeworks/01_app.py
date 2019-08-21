660c3fb2ee21bd7f870b50c4ca44227137dba5a0
archeranimesh <animeshb@archerimagine.com>
2019-08-21 21:12:25 +0530

    intial check-in for the project of the course.

--- code/07_webframeworks/01_app.py
+++ code/07_webframeworks/01_app.py
@@ -7,17 +7,22 @@
 # $env:FLASK_APP = "01_app.py"; flask run
 # $env:FLASK_APP = "01_app.py"; $env:FLASK_ENV = "development"; flask run
 from flask import Flask
+# this is for working with templates.
 from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello World!"
+    # no need to give the templates directory
+    name = "XYZ"
+    return render_template("index.html", name=name)
 
 @app.route("/french")
 def bounjour_world():
     return "Bonjour, World!"
+
+# https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
 
 # Paramater routing.
 @app.route("/name/<name>")
