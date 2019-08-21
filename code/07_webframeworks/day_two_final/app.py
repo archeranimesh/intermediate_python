8cc21910f2f27ab63504a1b4f06662a56e51e6d4
archeranimesh <animeshb@archerimagine.com>
2019-08-21 22:16:45 +0530

    completed the flask app also.

---
+++ code/07_webframeworks/day_two_final/app.py
@@ -0,0 +1,41 @@
+"""
+A Simple Flask web application interface
+For viewing popular Github repos sorted by stars using the
+Github search API.
+
+To run:
+MAC
+(env) FLASK_APP=app.py flask run
+(env) FLASK_APP=app.py FLASK_ENV=development flask run
+
+Windows
+$env:FLASK_APP = "app.py"; flask run
+$env:FLASK_APP = "app.py"; $env:FLASK_ENV = "development"; flask run
+"""
+from flask import Flask, render_template, request
+import repos
+
+app = Flask(__name__)
+
+available_languages = ["Python", "JavaScript", "Ruby", "Java"]
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    if request.method == 'GET':
+        # Use the list of all languages.
+        selected_languages = available_languages
+    elif request.method == "POST":
+        selected_languages = request.form.getlist("languages")
+
+    results = repos.api.repos_with_most_stars(selected_languages)
+
+    return render_template(
+        'index.html',
+        selected_languages=selected_languages,
+        available_languages=available_languages,
+        results=results
+    )
+
+@app.errorhandler(repos.exceptions.GitHubApiError)
+def handle_api_error(error):
+    return render_template('error.html', message=error)
